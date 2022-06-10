77feeba779f0a9158499a681cc22c0afdf6bb17c
halldor03 <kacoleker@gmail.com>
2022-06-10 19:03:48 +0200

    To do: scoring

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -5,33 +5,54 @@ function computerPlay() {
   console.log("Comp choice:" + result);
   return result;
 }
+
 function playerPlay() {
   let result = prompt("rock, paper or scissors?");
-  let result_lowercase = result.toLowerCase();
-  // to check my choice:
-  console.log("My choice:" + result_lowercase);
-  return result_lowercase;
+  if (typeof result == "string") {
+    let result2 = result.toLowerCase();
+    // to check my choice:
+    console.log("My choice:" + result2);
+    return result2;
+  } else {
+    // to check my choice:
+    console.log("My choice:" + result);
+    return result;
+  }
 }
 
 function playRound(computerSelection, playerSelection) {
   if (computerSelection == playerSelection) {
-    console.log("It's a draw!");
+    console.log("It's a draw! You both get 0 points");
+    return 0;
   } else if (
     (playerSelection == "scissors" && computerSelection == "paper") ||
     (playerSelection == "rock" && computerSelection == "scissors") ||
     (playerSelection == "paper" && computerSelection == "rock")
   ) {
-    console.log(`You win! ${playerSelection} beats ${computerSelection}!`);
+    console.log(
+      `You win the round! ${playerSelection} beats ${computerSelection}! You get 1 point`
+    );
+    return 1;
   } else if (
     (computerSelection == "scissors" && playerSelection == "paper") ||
     (computerSelection == "rock" && playerSelection == "scissors") ||
     (computerSelection == "paper" && playerSelection == "rock")
   ) {
-    console.log(`You lose! ${computerSelection} beats ${playerSelection}!`);
-  } else if (playerSelection == null || playerSelection == "") {
-    alert("You forfeited the game!");
+    console.log(
+      `You lose the round! ${computerSelection} beats ${playerSelection}! Opponent gets 1 point`
+    );
+    return -1;
+  } else if (playerSelection === null || playerSelection == "") {
+    alert("You forfeited the round!");
   } else {
     alert(`Wrong choice, must be "rock", "paper" or "scissors"!`);
   }
 }
-playRound(computerPlay(), playerPlay());
+
+function playGame(numberOfGames) {
+  for (let i = 0; i < numberOfGames; i++) {
+    playRound(computerPlay(), playerPlay());
+  }
+}
+// to play the game n times:
+playGame(1);
