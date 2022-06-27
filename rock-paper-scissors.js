b9a0f68fac7f78110664d8a8a582a84b6a77b2eb
halldor03 <kacoleker@gmail.com>
2022-06-27 23:11:56 +0200

    Started working on UI, to do:scoring and general css stuff

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,101 +1,87 @@
-function computerPlay() {
+const computerSelection = function computerPlay() {
   let choices = ["rock", "paper", "scissors"];
   let result = choices[Math.floor(Math.random() * choices.length)];
   // to check computer's choice:
-  console.log("Comp choice:" + result);
+  // console.log("Computer's choice:" + result);
   return result;
-}
+};
 
-function playerPlay() {
-  let result = prompt("rock, paper or scissors?");
-  if (typeof result == "string") {
-    let result2 = result.toLowerCase();
-    // to check your choice:
-    console.log("Your choice:" + result2);
-    return result2;
-  } else {
-    // to check your choice:
-    console.log("Your choice:" + result);
-    return result;
-  }
-}
-
-function playRound(computerSelection, playerSelection) {
+const playRound = function playRound(computerSelection, playerSelection) {
+  let currentResult = document.querySelector(".currentResult");
   if (computerSelection == playerSelection) {
-    console.log("It's a draw! You both get 0 points");
+    currentResult.textContent = `It's a draw! You both chose ${computerSelection} and get 0 points.`;
   } else if (
     (playerSelection == "scissors" && computerSelection == "paper") ||
     (playerSelection == "rock" && computerSelection == "scissors") ||
     (playerSelection == "paper" && computerSelection == "rock")
   ) {
-    console.log(
-      `You win the round, ${playerSelection} beats ${computerSelection}! You get 1 point`
-    );
+    currentResult.textContent = `You win the round, ${playerSelection} beats ${computerSelection}! You get 1 point.`;
   } else if (
     (computerSelection == "scissors" && playerSelection == "paper") ||
     (computerSelection == "rock" && playerSelection == "scissors") ||
     (computerSelection == "paper" && playerSelection == "rock")
   ) {
-    console.log(
-      `You lose the round, ${computerSelection} beats ${playerSelection}! Opponent gets 1 point`
-    );
-  } else if (playerSelection === null || playerSelection == "") {
-    console.log("You forfeited the round!");
-  } else {
-    console.log(
-      `Wrong choice, must be "rock", "paper" or "scissors", try again!`
-    );
-    return playerPlay();
+    currentResult.textContent = `You lose the round, ${computerSelection} beats ${playerSelection}! Opponent gets 1 point.`;
   }
-}
+};
 
-function scoring(
-  computerSelection,
-  playerSelection,
-  computerPoints,
-  playerPoints
-) {
-  if (
-    (playerSelection == "scissors" && computerSelection == "paper") ||
-    (playerSelection == "rock" && computerSelection == "scissors") ||
-    (playerSelection == "paper" && computerSelection == "rock")
-  ) {
-    playerPoints += 1;
-    console.log(`Your points: ${playerPoints}`);
-    console.log(`Computer points: ${computerPoints}`);
-  } else if (
-    (computerSelection == "scissors" && playerSelection == "paper") ||
-    (computerSelection == "rock" && playerSelection == "scissors") ||
-    (computerSelection == "paper" && playerSelection == "rock")
-  ) {
-    computerPoints += 1;
-    console.log(`Your points: ${playerPoints}`);
-    console.log(`Computer points: ${computerPoints}`);
-  }
-  return [computerPoints, playerPoints];
-}
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    // to check player's choice:
+    // console.log("Player's choice:" + button.id);
+    playRound(computerSelection(), button.id);
+  });
+});
 
-function playGame(numberOfGames) {
-  let computerPoints = 0;
-  let playerPoints = 0;
-  for (let i = 0; i < numberOfGames; i++) {
-    let computerSelection = computerPlay();
-    let playerSelection = playerPlay();
-    playRound(computerSelection, playerSelection);
-    [computerPoints, playerPoints] = scoring(
-      computerSelection,
-      playerSelection,
-      computerPoints,
-      playerPoints
-    );
-  }
-  if (playerPoints > computerPoints) {
-    alert(`You won the game ${playerPoints}:${computerPoints}!`);
-  } else if (playerPoints < computerPoints) {
-    alert(`You lost the game ${computerPoints}:${playerPoints}!`);
-  } else {
-    alert(`It's a draw ${computerPoints}:${playerPoints}!`);
-  }
-}
+// function scoring(
+//   computerSelection,
+//   playerSelection,
+//   computerPoints,
+//   playerPoints
+// ) {
+//   if (
+//     (playerSelection == "scissors" && computerSelection == "paper") ||
+//     (playerSelection == "rock" && computerSelection == "scissors") ||
+//     (playerSelection == "paper" && computerSelection == "rock")
+//   ) {
+//     playerPoints += 1;
+//     console.log(`Your points: ${playerPoints}`);
+//     console.log(`Computer points: ${computerPoints}`);
+//   } else if (
+//     (computerSelection == "scissors" && playerSelection == "paper") ||
+//     (computerSelection == "rock" && playerSelection == "scissors") ||
+//     (computerSelection == "paper" && playerSelection == "rock")
+//   ) {
+//     computerPoints += 1;
+//     console.log(`Your points: ${playerPoints}`);
+//     console.log(`Computer points: ${computerPoints}`);
+//   }
+//   return [computerPoints, playerPoints];
+// }
+
+// function playGame(numberOfGames) {
+//   let computerPoints = 0;
+//   let playerPoints = 0;
+//   for (let i = 0; i < numberOfGames; i++) {
+//     let computerSelection = computerPlay();
+//     let playerSelection = playerPlay();
+//     playRound(computerSelection, playerSelection);
+//     [computerPoints, playerPoints] = scoring(
+//       computerSelection,
+//       playerSelection,
+//       computerPoints,
+//       playerPoints
+//     );
+//   }
+//   if (playerPoints > computerPoints) {
+//     alert(`You won the game ${playerPoints}:${computerPoints}!`);
+//   } else if (playerPoints < computerPoints) {
+//     alert(`You lost the game ${computerPoints}:${playerPoints}!`);
+//   } else {
+//     alert(`It's a draw ${computerPoints}:${playerPoints}!`);
+//   }
+// }
+
 // to play the game n times:
-playGame(3);
+// playGame(3);
