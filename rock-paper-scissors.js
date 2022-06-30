43791b7ee0e2df97ab60bb737019c06176fe65e1
halldor03 <kacoleker@gmail.com>
2022-06-30 22:11:35 +0200

    Added more logic

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -25,6 +25,39 @@ function playRound(computerSelection, playerSelection) {
   }
 }
 
+function playGame() {
+  let computerPoints = 0;
+  let playerPoints = 0;
+  const buttons = document.querySelectorAll(".rock, .paper, .scissors");
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const playerSelection = button.classList;
+      const computerSelection = computerPlay();
+      // to check player's choice:
+      console.log("Player's choice:" + playerSelection);
+      playRound(computerSelection, playerSelection);
+      [computerPoints, playerPoints] = scoring(
+        computerSelection,
+        playerSelection,
+        computerPoints,
+        playerPoints
+      );
+      if (playerPoints == 5 || computerPoints == 5) {
+        if (playerPoints > computerPoints) {
+          let wonGame = document.querySelector(".resultGameWon");
+          wonGame.textContent = `You won the game ${playerPoints}:${computerPoints}! Press Enter to play again!`;
+        } else if (playerPoints < computerPoints) {
+          let lostGame = document.querySelector(".resultGameLost");
+          lostGame.textContent = `You lost the game ${computerPoints}:${playerPoints}! Press Enter to play again!`;
+        } else {
+          let drawGame = document.querySelector(".resultGameDraw");
+          drawGame.textContent = `It's a draw ${computerPoints}:${playerPoints}! Press Enter to play again!`;
+        }
+      }
+    });
+  });
+}
+
 function scoring(
   computerSelection,
   playerSelection,
@@ -37,76 +70,40 @@ function scoring(
     (playerSelection == "paper" && computerSelection == "rock")
   ) {
     playerPoints += 1;
-    console.log(`Your points: ${playerPoints}`);
-    console.log(`Computer points: ${computerPoints}`);
+    let currentPlayerScore = document.querySelector(".currentPlayerScore");
+    currentPlayerScore.textContent = `Your points: ${playerPoints}`;
+    let currentComputerScore = document.querySelector(".currentComputerScore");
+    currentComputerScore.textContent = `Computer points: ${computerPoints}`;
   } else if (
     (computerSelection == "scissors" && playerSelection == "paper") ||
     (computerSelection == "rock" && playerSelection == "scissors") ||
     (computerSelection == "paper" && playerSelection == "rock")
   ) {
     computerPoints += 1;
-    console.log(`Your points: ${playerPoints}`);
-    console.log(`Computer points: ${computerPoints}`);
+    let currentPlayerScore = document.querySelector(".currentPlayerScore");
+    currentPlayerScore.textContent = `Your points: ${playerPoints}`;
+    let currentComputerScore = document.querySelector(".currentComputerScore");
+    currentComputerScore.textContent = `Computer points: ${computerPoints}`;
+  } else {
+    let currentPlayerScore = document.querySelector(".currentPlayerScore");
+    currentPlayerScore.textContent = `Your points: ${playerPoints}`;
+    let currentComputerScore = document.querySelector(".currentComputerScore");
+    currentComputerScore.textContent = `Computer points: ${computerPoints}`;
   }
   return [computerPoints, playerPoints];
 }
 
-function playGame() {
-  const buttons = document.querySelectorAll(".rock, .paper, .scissors");
-  buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-      const playerSelection = button.classList;
-      const computerSelection = computerPlay();
-      // to check player's choice:
-      console.log("Player's choice:" + playerSelection);
-      playRound(computerSelection, playerSelection);
-      let computerPoints = 0;
-      let playerPoints = 0;
-      [computerPoints, playerPoints] = scoring(
-        computerSelection,
-        playerSelection,
-        computerPoints,
-        playerPoints
-      );
-      if (playerPoints > computerPoints) {
-        let wonRound = document.querySelector(".resultGameWon");
-        wonRound.textContent = `You won the game ${playerPoints}:${computerPoints}!`;
-      } else if (playerPoints < computerPoints) {
-        let lostRound = document.querySelector(".resultGameLost");
-        lostRound.textContent = `You lost the game ${computerPoints}:${playerPoints}!`;
-      } else {
-        let drawRound = document.querySelector(".resultGameDraw");
-        drawRound.textContent = `It's a draw ${computerPoints}:${playerPoints}!`;
-      }
-    });
-  });
-}
-
-const startButton = document.querySelector("#start");
-startButton.addEventListener("click", () => {
-  console.log("Starting the game...");
-  playGame();
+const startKey = document.querySelector(".startKey");
+let startMessage = document.querySelector(".startMessage");
+window.addEventListener("keydown", function (e) {
+  if (e.key == "Enter") {
+    startMessage.textContent = "The game has started!";
+    playGame();
+    startMessage.textContent = "";
+  }
 });
 
-// function playGame(numberOfGames) {
-//   let computerPoints = 0;
-//   let playerPoints = 0;
-//   for (let i = 0; i < numberOfGames; i++) {
-//     let computerSelection = computerPlay();
-//     let playerSelection = playerPlay();
-//     playRound(computerSelection, playerSelection);
-//     [computerPoints, playerPoints] = scoring(
-//       computerSelection,
-//       playerSelection,
-//       computerPoints,
-//       playerPoints
-//     );
-//   }
-//   if (playerPoints > computerPoints) {
-//     alert(`You won the game ${playerPoints}:${computerPoints}!`);
-//   } else if (playerPoints < computerPoints) {
-//     alert(`You lost the game ${computerPoints}:${playerPoints}!`);
-//   } else {
-//     alert(`It's a draw ${computerPoints}:${playerPoints}!`);
-//   }
-// }
+// TO DO:
+// - stop user from clicking "Enter" multiple TimeRanges,
+// - add function end when player/computer reaches 5 points,
+// - css
